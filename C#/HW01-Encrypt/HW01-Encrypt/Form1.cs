eeb83018e07633c43614345e0e1ea02de31257d3
Clooooode <jackey8616@users.noreply.github.com>
2018-03-09 02:51:14 +0800

    Using C# is the most idiotic idea to make a Algorithm stuff

---
+++ C#/HW01-Encrypt/HW01-Encrypt/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW01_Encrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String AesEncrypt(String inputData, String inputKey) {
+            string encrypt = "";
+            try
+            {
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+                byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+                aes.Key = key;
+                aes.IV = iv;
+
+                byte[] dataByteArray = Encoding.UTF8.GetBytes(inputData);
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(dataByteArray, 0, dataByteArray.Length);
+                    cs.FlushFinalBlock();
+                    encrypt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
+            return encrypt;
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = this.AesEncrypt(inputBox.Text, "key");
+        }
+    }
+}
