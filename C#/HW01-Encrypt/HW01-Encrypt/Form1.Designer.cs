a18fde1908990f87c66ae684f340b2ca78459b3b
Clooooode <jackey8616@users.noreply.github.com>
2018-03-10 14:02:05 +0800

    Decrypt & DES and some UI stuff
    but DES can't work yet, lol.

--- C#/HW01-Encrypt/HW01-Encrypt/Form1.Designer.cs
+++ C#/HW01-Encrypt/HW01-Encrypt/Form1.Designer.cs
@@ -30,7 +30,17 @@
         {
             this.inputBox = new System.Windows.Forms.TextBox();
             this.outputBox = new System.Windows.Forms.TextBox();
-            this.encryptBtn = new System.Windows.Forms.Button();
+            this.encryptAesBtn = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.decryptAesBtn = new System.Windows.Forms.Button();
+            this.swapBtn = new System.Windows.Forms.Button();
+            this.clearInputBtn = new System.Windows.Forms.Button();
+            this.clearOutputBtn = new System.Windows.Forms.Button();
+            this.clearKeyBtn = new System.Windows.Forms.Button();
+            this.clearAllBtn = new System.Windows.Forms.Button();
+            this.keyRandomBtn = new System.Windows.Forms.Button();
+            this.desEncryptBtn = new System.Windows.Forms.Button();
+            this.desDecryptBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // inputBox
@@ -38,33 +48,141 @@
             this.inputBox.Location = new System.Drawing.Point(42, 35);
             this.inputBox.Multiline = true;
             this.inputBox.Name = "inputBox";
-            this.inputBox.Size = new System.Drawing.Size(306, 91);
+            this.inputBox.Size = new System.Drawing.Size(537, 91);
             this.inputBox.TabIndex = 0;
             // 
             // outputBox
             // 
-            this.outputBox.Location = new System.Drawing.Point(42, 293);
+            this.outputBox.Location = new System.Drawing.Point(42, 265);
             this.outputBox.Multiline = true;
             this.outputBox.Name = "outputBox";
-            this.outputBox.Size = new System.Drawing.Size(306, 91);
+            this.outputBox.Size = new System.Drawing.Size(537, 91);
             this.outputBox.TabIndex = 1;
             // 
-            // encryptBtn
+            // encryptAesBtn
             // 
-            this.encryptBtn.Location = new System.Drawing.Point(42, 184);
-            this.encryptBtn.Name = "encryptBtn";
-            this.encryptBtn.Size = new System.Drawing.Size(75, 23);
-            this.encryptBtn.TabIndex = 2;
-            this.encryptBtn.Text = "Encrypt";
-            this.encryptBtn.UseVisualStyleBackColor = true;
-            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            this.encryptAesBtn.Location = new System.Drawing.Point(42, 184);
+            this.encryptAesBtn.Name = "encryptAesBtn";
+            this.encryptAesBtn.Size = new System.Drawing.Size(75, 23);
+            this.encryptAesBtn.TabIndex = 2;
+            this.encryptAesBtn.Text = "AES Encrypt";
+            this.encryptAesBtn.UseVisualStyleBackColor = true;
+            this.encryptAesBtn.Click += new System.EventHandler(this.encryptAesBtn_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(42, 132);
+            this.keyBox.Multiline = true;
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(456, 27);
+            this.keyBox.TabIndex = 3;
+            // 
+            // decryptAesBtn
+            // 
+            this.decryptAesBtn.Location = new System.Drawing.Point(42, 213);
+            this.decryptAesBtn.Name = "decryptAesBtn";
+            this.decryptAesBtn.Size = new System.Drawing.Size(75, 23);
+            this.decryptAesBtn.TabIndex = 4;
+            this.decryptAesBtn.Text = "AES Decrypt";
+            this.decryptAesBtn.UseVisualStyleBackColor = true;
+            this.decryptAesBtn.Click += new System.EventHandler(this.decryptAesBtn_Click);
+            // 
+            // swapBtn
+            // 
+            this.swapBtn.Location = new System.Drawing.Point(504, 184);
+            this.swapBtn.Name = "swapBtn";
+            this.swapBtn.Size = new System.Drawing.Size(75, 52);
+            this.swapBtn.TabIndex = 5;
+            this.swapBtn.Text = "Swap";
+            this.swapBtn.UseVisualStyleBackColor = true;
+            this.swapBtn.Click += new System.EventHandler(this.swapBtn_Click);
+            // 
+            // clearInputBtn
+            // 
+            this.clearInputBtn.Location = new System.Drawing.Point(342, 184);
+            this.clearInputBtn.Name = "clearInputBtn";
+            this.clearInputBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearInputBtn.TabIndex = 6;
+            this.clearInputBtn.Text = "Clear Input";
+            this.clearInputBtn.UseVisualStyleBackColor = true;
+            this.clearInputBtn.Click += new System.EventHandler(this.clearInputBtn_Click);
+            // 
+            // clearOutputBtn
+            // 
+            this.clearOutputBtn.Location = new System.Drawing.Point(342, 213);
+            this.clearOutputBtn.Name = "clearOutputBtn";
+            this.clearOutputBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearOutputBtn.TabIndex = 7;
+            this.clearOutputBtn.Text = "Clear Output";
+            this.clearOutputBtn.UseVisualStyleBackColor = true;
+            this.clearOutputBtn.Click += new System.EventHandler(this.clearOutputBtn_Click);
+            // 
+            // clearKeyBtn
+            // 
+            this.clearKeyBtn.Location = new System.Drawing.Point(423, 184);
+            this.clearKeyBtn.Name = "clearKeyBtn";
+            this.clearKeyBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearKeyBtn.TabIndex = 8;
+            this.clearKeyBtn.Text = "Clear Key";
+            this.clearKeyBtn.UseVisualStyleBackColor = true;
+            this.clearKeyBtn.Click += new System.EventHandler(this.clearKeyBtn_Click);
+            // 
+            // clearAllBtn
+            // 
+            this.clearAllBtn.Location = new System.Drawing.Point(423, 213);
+            this.clearAllBtn.Name = "clearAllBtn";
+            this.clearAllBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearAllBtn.TabIndex = 9;
+            this.clearAllBtn.Text = "Clear All";
+            this.clearAllBtn.UseVisualStyleBackColor = true;
+            this.clearAllBtn.Click += new System.EventHandler(this.clearAllBtn_Click);
+            // 
+            // keyRandomBtn
+            // 
+            this.keyRandomBtn.Location = new System.Drawing.Point(504, 132);
+            this.keyRandomBtn.Name = "keyRandomBtn";
+            this.keyRandomBtn.Size = new System.Drawing.Size(75, 27);
+            this.keyRandomBtn.TabIndex = 10;
+            this.keyRandomBtn.Text = "Random";
+            this.keyRandomBtn.UseVisualStyleBackColor = true;
+            this.keyRandomBtn.Click += new System.EventHandler(this.keyRandomBtn_Click);
+            // 
+            // desEncryptBtn
+            // 
+            this.desEncryptBtn.Location = new System.Drawing.Point(123, 184);
+            this.desEncryptBtn.Name = "desEncryptBtn";
+            this.desEncryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.desEncryptBtn.TabIndex = 11;
+            this.desEncryptBtn.Text = "DES Encrypt";
+            this.desEncryptBtn.UseVisualStyleBackColor = true;
+            this.desEncryptBtn.Click += new System.EventHandler(this.desEncryptBtn_Click);
+            // 
+            // desDecryptBtn
+            // 
+            this.desDecryptBtn.Location = new System.Drawing.Point(123, 213);
+            this.desDecryptBtn.Name = "desDecryptBtn";
+            this.desDecryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.desDecryptBtn.TabIndex = 12;
+            this.desDecryptBtn.Text = "DES Decrypt";
+            this.desDecryptBtn.UseVisualStyleBackColor = true;
+            this.desDecryptBtn.Click += new System.EventHandler(this.desDecryptBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.encryptBtn);
+            this.ClientSize = new System.Drawing.Size(622, 383);
+            this.Controls.Add(this.desDecryptBtn);
+            this.Controls.Add(this.desEncryptBtn);
+            this.Controls.Add(this.keyRandomBtn);
+            this.Controls.Add(this.clearAllBtn);
+            this.Controls.Add(this.clearKeyBtn);
+            this.Controls.Add(this.clearOutputBtn);
+            this.Controls.Add(this.clearInputBtn);
+            this.Controls.Add(this.swapBtn);
+            this.Controls.Add(this.decryptAesBtn);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.encryptAesBtn);
             this.Controls.Add(this.outputBox);
             this.Controls.Add(this.inputBox);
             this.Name = "Form1";
@@ -78,7 +196,17 @@
 
         private System.Windows.Forms.TextBox inputBox;
         private System.Windows.Forms.TextBox outputBox;
-        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button encryptAesBtn;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button decryptAesBtn;
+        private System.Windows.Forms.Button swapBtn;
+        private System.Windows.Forms.Button clearInputBtn;
+        private System.Windows.Forms.Button clearOutputBtn;
+        private System.Windows.Forms.Button clearKeyBtn;
+        private System.Windows.Forms.Button clearAllBtn;
+        private System.Windows.Forms.Button keyRandomBtn;
+        private System.Windows.Forms.Button desEncryptBtn;
+        private System.Windows.Forms.Button desDecryptBtn;
     }
 }
 
--- C#/HW01-Encrypt/HW01-Encrypt/Form1.cs
+++ C#/HW01-Encrypt/HW01-Encrypt/Form1.cs
@@ -47,9 +47,130 @@ namespace HW01_Encrypt
             return encrypt;
         }
 
-        private void encryptBtn_Click(object sender, EventArgs e)
+        public String AesDecrpt(String inputData, String inputKey) {
+            string decrypt = "";
+            try
+            {
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+                byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+                aes.Key = key;
+                aes.IV = iv;
+
+                byte[] dataByteArray = Convert.FromBase64String(inputData);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(dataByteArray, 0, dataByteArray.Length);
+                        cs.FlushFinalBlock();
+                        decrypt = Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
+            return decrypt;
+        }
+
+        public String DesEncrypt(String inpuData, String inputKey) {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+            byte[] dataByteArray = Encoding.UTF8.GetBytes(inpuData);
+
+            des.Key = key;
+            des.IV = iv;
+            string encrypt = "";
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(dataByteArray, 0, dataByteArray.Length);
+                cs.FlushFinalBlock();
+                encrypt = Convert.ToBase64String(ms.ToArray());
+            }
+            return encrypt;
+        }
+
+        public String DesDecrypt(String inputData, String inputKey) {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(inputKey));
+            des.Key = key;
+            des.IV = iv;
+
+            byte[] dataByteArray = Convert.FromBase64String(inputData);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(dataByteArray, 0, dataByteArray.Length);
+                    cs.FlushFinalBlock();
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+
+        private void encryptAesBtn_Click(object sender, EventArgs e)
         {
-            outputBox.Text = this.AesEncrypt(inputBox.Text, "key");
+            outputBox.Text = this.AesEncrypt(inputBox.Text, keyBox.Text);
+        }
+
+        private void decryptAesBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = this.AesDecrpt(inputBox.Text, keyBox.Text);
+        }
+
+        private void swapBtn_Click(object sender, EventArgs e)
+        {
+            String buff = inputBox.Text;
+            inputBox.Text = outputBox.Text;
+            outputBox.Text = buff;
+        }
+
+        private void clearInputBtn_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+        }
+
+        private void clearOutputBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = "";
+        }
+
+        private void clearKeyBtn_Click(object sender, EventArgs e)
+        {
+            keyBox.Text = "";
+        }
+
+        private void clearAllBtn_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+            outputBox.Text = "";
+            keyBox.Text = "";
+        }
+
+        private void keyRandomBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void desEncryptBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = DesEncrypt(inputBox.Text, keyBox.Text);
+        }
+
+        private void desDecryptBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = DesDecrypt(inputBox.Text, keyBox.Text);
         }
     }
 }
