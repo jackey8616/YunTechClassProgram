672485c429385e444e0dd83d11077c252c81d019
Clooooode <jackey8616@users.noreply.github.com>
2018-04-08 02:50:59 +0800

    Just a edge and init

---
+++ C#/HW02-MST/HW02-MST/Edge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW02_MST
+{
+    class Edge
+    {
+
+        private int src, dst, weight;
+
+        public Edge(int src, int dst, int weight) {
+            this.src = src;
+            this.dst = dst;
+            this.weight = weight;
+        }
+
+        public Edge(String[] str) {
+            this.src = int.Parse(str[0]);
+            this.dst = int.Parse(str[1]);
+            this.weight = int.Parse(str[2]);
+        }
+
+        override
+        public String ToString() {
+            return String.Format("SRC: {0:D}, DST: {1:D}, WEIGHT: {2:D}", this.src, this.dst, this.weight);
+        }
+    }
+}
---
+++ C#/HW02-MST/HW02-MST/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace HW02_MST
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Edge> edges = new List<Edge>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void initEdge(String input) {
+            MatchCollection matches = Regex.Matches(input, "([0-9]+,[0-9]+,[0-9]+)");
+            foreach (Match s in matches)
+                edges.Add(new Edge(s.ToString().Split(',')));
+            foreach (Edge e in edges)
+                Console.WriteLine(e.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                initEdge( sr.ReadToEnd());
+                sr.Close();
+            }
+        }
+    }
+}
