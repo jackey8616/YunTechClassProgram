fc04b703989923cea6527c84eb1642e47b187a80
clode <jackey8616@gmail.com>
2018-04-27 15:55:25 +0800

    IDIOT INLINE HW BY BJ

---
+++ C++/inline/inline.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+inline long double withInline(long double n) {
+    return n * n;
+}
+
+int main(void) {
+
+    int N = 0, exeN = 0;
+    cin >> N >> exeN;
+    long startTime = 0, endTime = 0, exeTime = 0;
+
+    cout << "N " << N << ", exeN " << exeN << endl;
+    for(int n = 0; n < N; n++) {
+        startTime = time(0);
+        cout << "\t| StartTime " << startTime << " | ";
+
+        for(int i = 0; i < exeN; ++i) {
+            long double result = withInline(i);
+            cout << result << endl;
+            //cout << "FACT(" << i << ")=" << result << endl;
+        }
+
+        endTime = time(0);
+        cout << "EndTime " << endTime << " | ";
+        exeTime = endTime - startTime;
+        cout << "ExeTime " << exeTime << endl;
+    }
+    return 0;
+}
+
